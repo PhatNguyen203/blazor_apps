3d4b55745c40113dc8c07de3a184caf20f660994
Lee0ne203 <Lee0ne203@gmail.com>
2025-09-07 18:43:48 -0500

    added wallet app

---
+++ Blazor.App/Models/Transaction.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.App.Models
+{
+	public class Transaction
+	{
+		public string Type { get; set; } = string.Empty; // "Deposit" or "Withdraw"
+		public decimal Amount { get; set; }
+		public DateTime Time { get; set; }
+	}
+}
---
+++ Blazor.App/Models/Wallet.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.App.Models
+{
+	public class Wallet
+	{
+		public decimal Balance { get; set; } = 0;
+		public List<Transaction> Transactions { get; set; } = new();
+	}
+}
